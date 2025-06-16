5581ca75787adaeda5c8f0f37501930b19ca28ee
boitumelo-09 <belmiredub4@gmail.com>
2025-06-16 22:38:36 +0200

    update

--- spvcode.cpp
+++ spvcode.cpp
@@ -8,173 +8,244 @@ struct Credential
     std::string username;
     std::string password;
 };
+
 const int maxCredentials = 100;
-void validator(std::string &password);
+void userAuthentication(std::string &password);
 void addCredential(Credential info[], int &userCount);
-void viewCredential(Credential info[], int &userCount);
-void deteleCredential(struct Credential (&info)[]);
-void passwordStrength(struct Credential (&info)[]);
+void viewCredential(Credential info[], int userCount);
+void deleteCredential(Credential info[], int &userCount);
+bool passwordStrength(Credential info[], int userCount);
+void displayMenu(int &option, Credential info[], int userCount);
+
 int main()
 {
-
     std::string vaultPassword;
     Credential credential[maxCredentials];
-    int usercount = 0;
+    int usercount = 0, menuOption;
+
     system("cls");
-    validator(vaultPassword);
-    int menuOption, flowOption;
-    std::cout << "Hello, Welcome To Boitumelo's Password Vault.\nYou are given the opportunity to save your credentials and have them for future usage\nPress Enter To Proceed...\n";
+    userAuthentication(vaultPassword);
+    displayMenu(menuOption, credential, usercount);
+    return 0;
+}
+void userAuthentication(std::string &password)
+{
+    while (true)
+    {
+        bool allDigit = true;
+        bool allequal = false;
+        std::cout << "Enter Your 4-digit Vault Access Code:";
+        getline(std::cin, password);
+        std::cout << "Press Enter To Confirm...";
+        std::cin.get();
+        system("cls");
+        for (char c : password)
+        {
+            if (!isdigit(c))
+            {
+                allDigit = false;
+                break;
+            }
+            if (c == c)
+            {
+                allequal = true;
+                break;
+            }
+        }
+        if (allDigit == false)
+        {
+            std::cout << "Invalid Input.\nThe Access Code You've Entered Contains Letters AND That Isn't Allowed.\nPress Enter To Restart Vault...";
+            std::cin.get();
+            system("cls");
+        }
+        else if (password.size() != 4)
+        {
+            std::cout << "Invalid Input.\nThe Access Code You've Entered Must Be 4-DIGITS.\nPress Enter To Restart Vault...";
+            std::cin.get();
+            system("cls");
+        }
+        /* else if (allequal == true)
+        {
+            std::cout << "Security Error!!.\nThe Access Code You've Entered Must Not Contain The Same Digits.\nPress Enter To Restart Vault...";
+            std::cin.get();
+            system("cls");
+        } */
 
+        else
+        {
+            std::cout << "Access Granted\nPress Enter To Continue...";
+            std::cin.get();
+            system("cls");
+            break;
+        }
+    }
+}
+void addCredential(Credential info[], int &userCount)
+{
+    if (userCount > maxCredentials)
+    {
+        std::cout << "The Vault Is Full\nPress Enter To Continue...";
+        std::cin.get();
+    }
+    else
+    {
+        std::cin.ignore();
+        std::cout << "|     Adding Credentials     |\n\n";
+        std::cout << "Website Name :";
+        getline(std::cin, info[userCount].website);
+        std::cout << std::endl;
+        std::cout << "Username     : ";
+        getline(std::cin, info[userCount].username);
+        std::cout << std::endl;
+        std::cout << "Password     : ";
+        getline(std::cin, info[userCount].password);
+        std::cout << std::endl;
+        std::cout << "\nPress Enter To Save Credentials...";
+        std::cin.get();
+        system("cls");
+        std::cout << "Credentials Have Been Successfully Added\nPress Enter To Return To Menu...";
+        userCount++;
+        std::cin.get();
+        system("cls");
+    }
+};
+void displayMenu(int &option, Credential info[], int userCount)
+{
+    std::cout << "Welcome To The Vault\nPress Enter To View Options...";
     std::cin.get();
-
+    system("cls");
     do
     {
-        system("cls");
 
-        std::cout << "Welcome to Your Vault\n";
-
-        std::cout << "Options\n";
-        std::cout << "1. Add New Credential\n2. View All Saved Credentials\n3. Delete A Credential\n4. Check Password Strength\n5. Exit\nOption:";
-
-        std::cin >> menuOption;
-        switch (menuOption)
+        std::cout << "1. Add Credential\n2. View Saved Credentials\n3. Delete Credential\n4. Check Password Strength\n5. Exit\n  Option:";
+        std::cin >> option;
+        switch (option)
         {
         case 1:
-            addCredential(credential, usercount);
+            system("cls");
+            addCredential(info, userCount);
             break;
         case 2:
-            viewCredential(credential, usercount);
+            system("cls");
+            viewCredential(info, userCount);
             break;
         case 3:
-            deteleCredential(credential);
+            system("cls");
+            deleteCredential(info, userCount);
             break;
         case 4:
-            passwordStrength(credential);
+            system("cls");
+            viewCredential(info, userCount);
             break;
         case 5:
             system("cls");
-            std::cout << "Thank You For Visisting.\nExiting Vault\n";
-            return 0;
+            std::cout << "Exiting Vault...";
+            return;
             break;
-
         default:
+        {
             system("cls");
-            std::cout << "Invalid Input\n1. Restart Vault\n2. Exit Vault\n";
-            std::cout << "Option:";
-            std::cin >> flowOption;
-            if (flowOption == 1)
+            std::cout << "Invalid Input\n   Options\n1. Restart Vault\n2.Exit Vault\n Option:";
+            std::cin >> option;
+            switch (option)
+            {
+            case 1:
             {
                 system("cls");
-                std::cout << "Restarting Vault...\n";
-                std::cout << "Press Enter To Continue...";
-                std::cin.ignore(); // Clear the newline character left in the input buffer
-                std::cin.get();    // Wait for user to press Enter
-
-                return main();
-                break;
-            }
-            else if (flowOption == 2)
-            {
-                system("cls");
-                std::cout << "Thank You For Visisting.\nExiting Vault...\n";
-                return 0;
+                std::cout << "Restarting Vault...\nPress Enter To Continue.";
+                std::cin.get();
+                displayMenu(option, info, userCount);
                 break;
             }
 
-            else
+            case 2:
             {
                 system("cls");
-                std::cout << "Invalid Input\n";
+                std::cout << "Exiting Vault...";
+                return;
                 break;
             }
+
+            default:
+            {
+
+                system("cls");
+                std::cout << "User INPUT ERROR\nRestarting Vault...\nPress Enter To Continue.";
+                std::cin.get();
+                displayMenu(option, info, userCount);
+                break;
+            }
+            }
+            break;
         }
-    } while (menuOption < 5 && menuOption > 0);
+        }
 
-    return 0;
-}
-void validator(std::string &password)
+    } while (option > 0 && option < 6);
+};
+void viewCredential(Credential info[], int userCount)
 {
 
-    while (true)
+    if (userCount == 0)
     {
-
-        std::cout << "Enter Your Vault Password:";
-        std::getline(std::cin, password);
-        if (password.length() > 7)
+        std::cin.ignore();
+        std::cout << "The Vault Is Empty\nPress Enter To Return To Menu...";
+        std::cin.get();
+        system("cls");
+    }
+    else
+    {
+        std::cin.ignore();
+        std::cout << "Saved Credentials\n";
+        for (int i = 0; i < userCount; i++)
         {
-            password[0] = toupper(password[0]);
-            break;
+            std::cout << (i + 1) << ".   |  " << info[i].website << "   |  " << info[i].username << "   |  " << info[i].password << std::endl;
+        }
+        std::cout << "\nPress Enter To Return To Menu...";
+        std::cin.get();
+        system("cls");
+    }
+}
+void deleteCredential(Credential info[], int &userCount)
+{
+    int deleteEntry;
+    if (userCount == 0)
+    {
+        std::cin.ignore();
+        std::cout << "The Vault Is Empty\nPress Enter To Return To Menu...";
+        std::cin.get();
+        system("cls");
+    }
+    else
+    {
+        std::cin.ignore();
+        std::cout << "Saved Credentials\n";
+        for (int i = 0; i < userCount; i++)
+        {
+            std::cout << (i + 1) << ".   |  " << info[i].website << "   |  " << info[i].username << "   |  " << info[i].password << std::endl;
+        }
+        std::cout << "\nEnter The Number Of The Credentials You Want To Delete:";
+        std::cin >> deleteEntry;
+        deleteEntry--;
+
+        if (deleteEntry < 0 || deleteEntry > maxCredentials)
+        {
+            std::cin.ignore();
+            std::cout << "The Entry Entered Is Invalid\nPress Enter To Return To Menu...";
+            std::cin.get();
+            system("cls");
         }
         else
         {
+            for (int i = deleteEntry; i < userCount; i++)
+            {
+                info[i] = info[i + 1];
+            }
+
+            std::cout << "Entry Has Been Successfully Deleted\nPress Enter To Return To Menu...";
+            std::cin.get();
             system("cls");
-            std::cout << "Password Must Have More Than 7 Characters\n";
-            validator(password);
-            break;
+            userCount--;
         }
     }
 }
-
-void addCredential(Credential info[], int &userCount)
-{
-    if (userCount >= maxCredentials)
-    {
-        std::cout << "Vault is full, cannot add more credentials.\n";
-        return;
-    }
-    else
-    {
-        system("cls");
-        std::cout << "Adding Credential\n";
-        std::cout << "You can add up to " << maxCredentials << " credentials.\n";
-        std::cout << "Current number of credentials: " << userCount << "\n";
-        std::cout << "Press Enter to continue...\n";
-        std::cin.get(); // Wait for user to press Enter
-        system("cls");
-        std::cin.ignore();
-        std::cout << std::setw(20) << "Adding Credential\n";
-        std::cout << "Enter Website:";
-        getline(std::cin, info[userCount].website);
-        std::cout << std::endl;
-        std::cout << "Enter Username:";
-        getline(std::cin, info[userCount].username);
-        std::cout << std::endl;
-        std::cout << "Enter Password:";
-        getline(std::cin, info[userCount].password);
-        std::cout << std::endl;
-        userCount++;
-
-        std::cout << "Credential Added Successfully!\n";
-        std::cout << "Press Enter To Continue...";
-        std::cin.get(); // Wait for user to press Enter
-    }
-}
-void viewCredential(Credential info[], int &userCount)
-{
-    system("cls");
-    if (userCount == 0)
-    {
-        std::cout << "Vault Is Empty Bro";
-        return;
-    }
-
-    else
-    {
-        std::cout << "Saved Credentials";
-        for (auto i = 0; i < userCount; i++)
-        {
-            std::cout << "Credential " << (i + 1) << ":\nWebsite: " << info[i].website << "\nUsername: " << info[i].username << "\nPassword: " << info[i].password;
-            std::cout << std::endl;
-        }
-    }
-}
-void deteleCredential(struct Credential (&info)[])
-{
-    system("cls");
-    std::cout << "Deleting Credential";
-}
-void passwordStrength(struct Credential (&info)[])
-{
-    system("cls");
-    std::cout << "Checking Password Strength";
-}
+bool passwordStrength(Credential info[], int userCount);
